79171fc71e8c3f2994f05763863c9b9638ab7941
enumcase <markdaribayev@gmail.com>
2021-03-20 21:34:26 +0600

    added data loading VC

---
+++ Tripper/ViewControllers/DataLoadingViewController.swift
@@ -0,0 +1,49 @@
+//
+//  DataLoadingViewController.swift
+//  Tripper
+//
+//  Created by Margulan Daribayev on 20.03.2021.
+//
+
+import UIKit
+
+class DataLoadingViewController: UIViewController {
+    
+    var containerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.5) {
+            self.containerView.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            self.containerView.removeFromSuperview()
+            self.containerView = nil
+        }
+    }
+    
+
+}
