9297a23b180762e97d5da54d8d077de124f99a98
enumcase <markdaribayev@gmail.com>
2021-03-20 23:46:41 +0600

    added persistence error messages in request error

--- Tripper/Managers/RequestError.swift
+++ Tripper/Managers/RequestError.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 
-enum NetworkManagerError: String, Error {
+enum RequestError: String, Error {
+    // Network manager error messages
     case invalidURL = "The provided URL for fetching data is invalid."
     case unableToComplete = "Unable to complete your request. Please check your internet connection."
     case invalidResponse = "Invalid response from the server. Please try again."
     case invalidData = "The data received from the server was invalid. Please try again."
+    
+    // Persistence manager error messages
+    case unableToBookmark = "There was an error bookmarking the trip. Please try again."
+    case alreadyInBookmarks = "This trip is already in bookmarks."
 }
