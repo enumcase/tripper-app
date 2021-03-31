50eb9bfa8d4e45a41d6e083696aa696433b4028e
enumcase <markdaribayev@gmail.com>
2021-03-31 20:34:05 +0600

    added testing trips and bookmark action handler

--- Tripper/ViewControllers/Core/TripsViewController.swift
+++ Tripper/ViewControllers/Core/TripsViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class TripsViewController: DataLoadingViewController {
     
+    private lazy var tripsForTestingPurposes: [Trip] = [
+        Trip(season: "Spring", name: "Kyoto", year: 2021, duration: 14, follower: [Follower(followerId: 1, name: "Kazuya", image: "kazuya")], image: "kyoto"),
+        Trip(season: "Summer", name: "Tokyo", year: 2021, duration: 7, follower: [Follower(followerId: 1, name: "Kazuya", image: "kazuya"), Follower(followerId: 2, name: "Kayden", image: "kayden")], image: "tokyo"),
+        Trip(season: "Fall", name: "Nur-Sultan", year: 2021, duration: nil, follower: [Follower(followerId: 1, name: "Kazuya", image: "kazuya"), Follower(followerId: 2, name: "Kayden", image: "kayden")], image: "astana"),
+        Trip(season: "Winter", name: "London", year: 2021, duration: 20, follower: [Follower(followerId: 1, name: "Kazuya", image: "kazuya"), Follower(followerId: 2, name: "Kayden", image: "kayden")], image: "london")
+    ]
+    
     private var trips: [Trip] = []
     private var summerTrips: [Trip] = []
     private var springTrips: [Trip] = []
     private var fallTrips: [Trip] = []
     private var winterTrips: [Trip] = []
+    
+    private var tripsToShowInTableView: [Trip] = []
     
     private lazy var tripsTableView: UITableView = {
         let tableView = UITableView()
@@ -42,7 +51,12 @@ class TripsViewController: DataLoadingViewController {
         configButtons()
         setTableViewConstraints()
         
-        getTripsData()
+        #warning("uncomment after rebuilding database")
+        //getTripsData()
+        
+        getSeasonTrips()
+        
+        tripsToShowInTableView = tripsForTestingPurposes
     }
     
     private func getTripsData() {
@@ -66,20 +80,20 @@ class TripsViewController: DataLoadingViewController {
     }
     
     private func getSeasonTrips() {
-        summerTrips = trips.filter({ (trip) -> Bool in
+        summerTrips = tripsForTestingPurposes.filter({ (trip) -> Bool in
             trip.season == "Summer"
         })
         
-        springTrips = trips.filter({ (trip) -> Bool in
+        springTrips = tripsForTestingPurposes.filter({ (trip) -> Bool in
             trip.season == "Spring"
         })
         
-        fallTrips = trips.filter({ (trip) -> Bool in
-            trip.season == "Summer"
+        fallTrips = tripsForTestingPurposes.filter({ (trip) -> Bool in
+            trip.season == "Fall"
         })
         
-        winterTrips = trips.filter({ (trip) -> Bool in
-            trip.season == "Summer"
+        winterTrips = tripsForTestingPurposes.filter({ (trip) -> Bool in
+            trip.season == "Winter"
         })
     }
     
@@ -93,39 +107,51 @@ class TripsViewController: DataLoadingViewController {
     /// Button functions
     
     @objc private func didTapSummerButton() {
-        for button in topMenuBar.arrangedSubviews as! [UIButton] {
+        for button in topMenuBar.arrangedSubviews as! [UIButton] where button != topMenuBar.summerButton {
             button.isSelected = false
             button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         }
         
         topMenuBar.summerButton.isSelected.toggle()
+        tripsToShowInTableView = topMenuBar.summerButton.isSelected ? summerTrips : tripsForTestingPurposes
+        
+        tripsTableView.reloadData()
     }
     
     @objc private func didTapSpringButton() {
-        for button in topMenuBar.arrangedSubviews as! [UIButton] {
+        for button in topMenuBar.arrangedSubviews as! [UIButton] where button != topMenuBar.springButton {
             button.isSelected = false
             button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         }
         
         topMenuBar.springButton.isSelected.toggle()
+        tripsToShowInTableView = topMenuBar.springButton.isSelected ? springTrips : tripsForTestingPurposes
+        
+        tripsTableView.reloadData()
     }
     
     @objc private func didTapFallButton() {
-        for button in topMenuBar.arrangedSubviews as! [UIButton] {
+        for button in topMenuBar.arrangedSubviews as! [UIButton] where button != topMenuBar.fallButton {
             button.isSelected = false
             button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         }
         
         topMenuBar.fallButton.isSelected.toggle()
+        tripsToShowInTableView = topMenuBar.fallButton.isSelected ? fallTrips : tripsForTestingPurposes
+        
+        tripsTableView.reloadData()
     }
     
     @objc private func didTapWinterButton() {
-        for button in topMenuBar.arrangedSubviews as! [UIButton] {
+        for button in topMenuBar.arrangedSubviews as! [UIButton] where button != topMenuBar.winterButton {
             button.isSelected = false
             button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         }
 
         topMenuBar.winterButton.isSelected.toggle()
+        tripsToShowInTableView = topMenuBar.winterButton.isSelected ? winterTrips : tripsForTestingPurposes
+        
+        tripsTableView.reloadData()
     }
     
     /// Constraints
@@ -155,7 +181,7 @@ class TripsViewController: DataLoadingViewController {
 extension TripsViewController: UITableViewDataSource, UITableViewDelegate {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return trips.count
+        return tripsToShowInTableView.count
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
@@ -169,10 +195,29 @@ extension TripsViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: TripCardCell.reuseIdentifier, for: indexPath) as! TripCardCell
-        let trip = trips[indexPath.section]
+        cell.delegate = self
+        let trip = tripsToShowInTableView[indexPath.section]
         cell.setCardData(for: trip)
-//        if cell.isBookmarked
         return cell
+    }
+    
+    func handleBookmarkAction(for cell: TripCardCell) {
+        
+        guard let section = tripsTableView.indexPath(for: cell)?.section else {
+            return
+        }
+        
+        print(tripsToShowInTableView[section])
+        
+        PersistenceManager.updateWith(bookmarkedTrip: tripsToShowInTableView[section], actionType: .add) {
+            [weak self] error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                self.PresentAlertOnMainThread(title: "Something went wrong.", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
