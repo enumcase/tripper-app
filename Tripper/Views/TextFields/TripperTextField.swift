a977b894e164e525ef03e4efab2dfdf572036d4c
enumcase <markdaribayev@gmail.com>
2021-06-04 11:07:08 +0500

    Added keyboardType parameter for the main text field

--- Tripper/Views/TextFields/TripperTextField.swift
+++ Tripper/Views/TextFields/TripperTextField.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class TripperTextField: UITextField {
 
-    init(placeholder: String) {
+    init(placeholder: String, keyboardType: UIKeyboardType = .default) {
         super.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
         self.placeholder = placeholder
+        self.keyboardType = keyboardType
+        
+//        let bottomLine = CALayer()
+//        bottomLine.frame = CGRect(x: 0, y: self.frame.height - 1, width: self.frame.width, height: 1)
+//        bottomLine.backgroundColor = UIColor.white.cgColor
+//        self.borderStyle = .none
+//        self.layer.addSublayer(bottomLine)
     }
     
     required init?(coder: NSCoder) {
