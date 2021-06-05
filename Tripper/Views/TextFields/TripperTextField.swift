ff9b6b80dc56d5261c48cd36d4dc3e84b521ea4a
enumcase <markdaribayev@gmail.com>
2021-06-05 10:06:45 +0500

    Added padding for placeholder

--- Tripper/Views/TextFields/TripperTextField.swift
+++ Tripper/Views/TextFields/TripperTextField.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class TripperTextField: UITextField {
+    
+    private let padding = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
 
     init(placeholder: String, keyboardType: UIKeyboardType = .default) {
         super.init(frame: .zero)
@@ -15,11 +17,21 @@ class TripperTextField: UITextField {
         self.placeholder = placeholder
         self.keyboardType = keyboardType
         
-//        let bottomLine = CALayer()
-//        bottomLine.frame = CGRect(x: 0, y: self.frame.height - 1, width: self.frame.width, height: 1)
-//        bottomLine.backgroundColor = UIColor.white.cgColor
-//        self.borderStyle = .none
-//        self.layer.addSublayer(bottomLine)
+        heightAnchor.constraint(equalToConstant: 56).isActive = true
+        
+        self.useUnderline()
+    }
+    
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
     }
     
     required init?(coder: NSCoder) {
