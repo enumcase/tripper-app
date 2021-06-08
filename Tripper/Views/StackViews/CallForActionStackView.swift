2c6d7684199d171769f67ab98a105b941dd30243
enumcase <markdaribayev@gmail.com>
2021-06-08 20:16:19 +0600

    Created call for action label and button inside stack view

---
+++ Tripper/Views/StackViews/CallForActionStackView.swift
@@ -0,0 +1,48 @@
+//
+//  CallForActionStackView.swift
+//  Tripper
+//
+//  Created by Margulan Daribayev on 08.06.2021.
+//
+
+import UIKit
+
+class CallForActionStackView: UIStackView {
+
+    private let questionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        return label
+    }()
+    
+    let actionButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitleColor(.systemBlue, for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        return btn
+    }()
+    
+    init(questionText: String, actionText: String) {
+        super.init(frame: .zero)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        addArrangedSubview(questionLabel)
+        addArrangedSubview(actionButton)
+        
+        questionLabel.text = questionText
+        actionButton.setTitle(actionText, for: .normal)
+        
+        spacing = -4
+        axis = .horizontal
+        alignment = .firstBaseline
+        
+        questionLabel.setContentHuggingPriority(UILayoutPriority(rawValue: 251), for: .horizontal)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
