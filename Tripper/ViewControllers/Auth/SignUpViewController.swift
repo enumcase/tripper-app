f8a06f42d8a31def4146a03e52a586cb7114dd68
enumcase <markdaribayev@gmail.com>
2021-06-11 19:42:30 +0600

    Set the constraints for balloonImageView in sign up page

--- Tripper/ViewControllers/Auth/SignUpViewController.swift
+++ Tripper/ViewControllers/Auth/SignUpViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    private let balloonImageView = AuthPageImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,6 +19,21 @@ class SignUpViewController: UIViewController {
         navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         navigationItem.largeTitleDisplayMode = .always
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        view.addSubview(balloonImageView)
+        
+        setConstraints()
+    }
+    
+    /// Constraints
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            balloonImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            balloonImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            balloonImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            balloonImageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 50)
+        ])
     }
 
 }
