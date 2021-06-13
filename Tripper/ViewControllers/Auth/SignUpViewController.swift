afdb0d901e7a3593654326adffb3db962b6da8a1
enumcase <markdaribayev@gmail.com>
2021-06-13 18:13:08 +0600

    Added text fields to signup page

--- Tripper/ViewControllers/Auth/SignUpViewController.swift
+++ Tripper/ViewControllers/Auth/SignUpViewController.swift
@@ -13,6 +13,12 @@ class SignUpViewController: UIViewController {
     
     private let signUpLabel = AuthPageTextLabel(text: "Sign up to experience the joy\nof trips with other people!")
     
+    private let nameTextField = TripperTextField(placeholder: "Name")
+    
+    private let emailTextField = TripperTextField(placeholder: "Email", keyboardType: .emailAddress)
+    
+    private let passwordTextField = TripperTextField(placeholder: "Password", isSecureTextEntry: true)
+    
     private let signUpButton = TripperMainButton(title: "Sign Up")
     
     override func viewDidLoad() {
@@ -25,8 +31,11 @@ class SignUpViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         
         view.addSubview(balloonImageView)
-        view.addSubview(signUpButton)
         view.addSubview(signUpLabel)
+        view.addSubview(nameTextField)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(signUpButton)
         
         setConstraints()
     }
@@ -40,13 +49,25 @@ class SignUpViewController: UIViewController {
             balloonImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             balloonImageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 50),
             
-            signUpButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -118),
-            signUpButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            signUpButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            
             signUpLabel.topAnchor.constraint(equalTo: balloonImageView.bottomAnchor, constant: 16),
             signUpLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
-            signUpLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -48)
+            signUpLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -48),
+            
+            nameTextField.topAnchor.constraint(equalTo: signUpLabel.bottomAnchor, constant: 40),
+            nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            emailTextField.topAnchor.constraint(equalTo: nameTextField.bottomAnchor),
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor),
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            signUpButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -118),
+            signUpButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            signUpButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ])
     }
 
